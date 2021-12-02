4a98b2e2e99d6e40b506e8867c5997da6c268389
Harilaci01 <harsanyi.laszlo.ferenc.2017ice@bankitatabanya.hu>
2021-12-02 10:29:05 +0100

    Tőbb kép feltöltése 2

--- upload.php
+++ upload.php
@@ -1,14 +1,11 @@
 <?php 
-if(isset($_FILES["fileToUpload"])&&isset($_FILES["fileToUpload2"])){
+if(isset($_FILES["fileToUpload"])&&isset($_FILES["fileToUpload2"])||isset($_FILES["fileToUpload"])||isset($_FILES["fileToUpload2"])){
     $target_dir="uploads/";
     $target_file=$target_dir.basename($_FILES["fileToUpload"]["name"]);
-   $target_file2=$target_dir.basename($_FILES["fileToUpload2"]["name"]);
-    if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
-        echo "File 1 has been uploaded.";
-    }
-    if(move_uploaded_file($_FILES["fileToUpload2"]["tmp_name"], $target_file2)){
-        echo "<br> File 2 has been uploaded.";
-    }
+    $target_file2=$target_dir.basename($_FILES["fileToUpload2"]["name"]);
+    if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)||move_uploaded_file($_FILES["fileToUpload2"]["tmp_name"], $target_file2)){
+        echo "The file(s) has/have been uploaded.";
+    }   
 
 }
 ?>
@@ -16,7 +13,7 @@ if(isset($_FILES["fileToUpload"])&&isset($_FILES["fileToUpload2"])){
 <html>
     <body>
         <form action="upload.php" method="post" enctype="multipart/form-data">
-            Select image to upload:
+            Select image(s) to upload:
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="file" name="fileToUpload2" id="fileToUpload2">
             <input type="submit" value="Upload Image" name="submit">
