37aa19c7ef0bb9e8abe6d395eb93ad02525111be
Harilaci01 <harsanyi.laszlo.ferenc.2017ice@bankitatabanya.hu>
2022-02-10 12:21:14 +0100

    Biztositás próba 1

--- ajax.php
+++ ajax.php
@@ -2,10 +2,13 @@
 require 'includes/db.inc.php';
 
 if(isset($_REQUEST['keres'])){
-    $sql="SELECT nev FROM ulesrend WHERE nev LIKE ('%".$REQUEST['keres']."%')";
-    if($result=$conn->query($sql)){
-        if($result->num_rows>0){
-            while($row=$result->fetch_assoc()){
+    $stmt=$sql->prepare("SELECT nev FROM ulesrend WHERE nev LIKE ('% ? %')");
+    $stmt->bind_param('s', $_REQUEST['keres']);
+    $stmt->execute();
+    $stmt->store_result();
+    if($stmt){
+        if($stmt->num_rows()>0){
+            while($row=$stmt->fetch_assoc()){
                 echo $row['nev']."<br>";
             }
         }else echo "A keresett személy nem szerepel az adatbázisban.";
